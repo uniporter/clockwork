07802d58c6999c0a42fd48e854e5192ff181a431
Yi Hong <hongyiweiwu@outlook.com>
2019-12-31 23:07:46 +0900

    Added locale data backbones.

---
+++ lib/src/locale/locale.dart
@@ -0,0 +1,18 @@
+abstract class LocaleData {
+    static const LOCALE_DATA = "Locale Data";
+    static final Map<String, Locale> data = Map();
+
+    static String _dataPath = "";
+
+    static set setDataPath(String dataPath) {
+        _dataPath = dataPath;
+    }
+
+    static load(String localeName) {
+        
+    }
+}
+
+class Locale {
+
+}
